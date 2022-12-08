7dfbc1024dcd8b10302c12b9b7108596b5788674
Vojta <zapletal.vojta@gmail.com>
2022-12-08 10:08:35 +0100

    Slight adjustments, still little bit issue with orderid in one session

--- MainViewModel.cs
+++ MainViewModel.cs
@@ -108,7 +108,7 @@ public class MainViewModel : INotifyPropertyChanged
         
         _databaseServices.Add(finalOrder);
         _databaseServices.SaveChangesAsync();
-        
+
         LoadOrders();
     }
 
